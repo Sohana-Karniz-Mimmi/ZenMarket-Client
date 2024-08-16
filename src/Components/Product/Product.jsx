50cd469d6037b9c8dfa4e98a0dc9adeec8da6ca2
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-08-16 18:54:03 +0600

    added header

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -11,28 +11,42 @@ const Product = () => {
   const [sort, setSort] = useState('')
   const [search, setSearch] = useState('')
   const [searchText, setSearchText] = useState('')
-  const [jobs, setJobs] = useState([])
-  
-  
+  const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState()
+
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const { data } = await axios(
+          `${import.meta.env.VITE_API_URL}/products`
+        );
+        // Extract unique categories
+        const uniqueCategories = [...new Set(data.map(product => product.category))];
+        setCategories(uniqueCategories);
+      } catch (error) {
+        console.error('Error fetching data', error);
+      }
+    };
+    getData();
+  }, []);
+
   useEffect(() => {
     const getData = async () => {
       const { data } = await axios(
-        `${
-          import.meta.env.VITE_API_URL
-        }/all-jobs?page=${currentPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}&search=${search}`
+        `${import.meta.env.VITE_API_URL
+        }/all-products?page=${currentPage}&size=${itemsPerPage}&filter=${filter}&sort=${sort}&search=${search}`
       )
-      setJobs(data)
+      setProducts(data)
     }
     getData()
   }, [currentPage, filter, itemsPerPage, search, sort])
-  
-  
+
   useEffect(() => {
     const getCount = async () => {
       const { data } = await axios(
-        `${
-          import.meta.env.VITE_API_URL
-        }/jobs-count?filter=${filter}&search=${search}`
+        `${import.meta.env.VITE_API_URL
+        }/products-count?filter=${filter}&search=${search}`
       )
 
       setCount(data.count)
@@ -62,11 +76,12 @@ const Product = () => {
     setSearch(searchText)
   }
 
-  console.log(search)
   return (
     <div className='container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between'>
       <div>
         <div className='flex flex-col md:flex-row justify-center items-center gap-5 '>
+
+          {/* Filter */}
           <div>
             <select
               onChange={e => {
@@ -79,12 +94,51 @@ const Product = () => {
               className='border p-4 rounded-lg'
             >
               <option value=''>Filter By Category</option>
+              {
+                categories?.map((category, index) => (
+                  <option key={index} value={category}>{category}</option>
+                ))
+              }
+            </select>
+          </div>
+
+          <div>
+            <select
+              onChange={e => {
+                setFilter(e.target.value)
+                setCurrentPage(1)
+              }}
+              value={filter}
+              name='category'
+              id='category'
+              className='border p-4 rounded-lg'
+            >
+              <option value=''>Filter By Brand</option>
               <option value='Brand'>Brand</option>
               <option value='Category'>Category</option>
               <option value='Price Range'>Price Range</option>
             </select>
           </div>
 
+          <div>
+            <select
+              onChange={e => {
+                setFilter(e.target.value)
+                setCurrentPage(1)
+              }}
+              value={filter}
+              name='category'
+              id='category'
+              className='border p-4 rounded-lg'
+            >
+              <option value=''>Filter By Price Range</option>
+              <option value='Brand'>Brand</option>
+              <option value='Category'>Category</option>
+              <option value='Price Range'>Price Range</option>
+            </select>
+          </div>
+
+          {/* Search */}
           <form onSubmit={handleSearch}>
             <div className='flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300'>
               <input
@@ -102,6 +156,8 @@ const Product = () => {
               </button>
             </div>
           </form>
+
+          {/* Sort */}
           <div>
             <select
               onChange={e => {
@@ -113,21 +169,37 @@ const Product = () => {
               id='sort'
               className='border p-4 rounded-md'
             >
-              <option value=''>Sort Products By</option>
+              <option value=''>Sort By Date</option>
+              <option value='dsc'>Newest Product</option>
+            </select>
+          </div>
+
+          <div>
+            <select
+              onChange={e => {
+                setSort(e.target.value)
+                setCurrentPage(1)
+              }}
+              value={sort}
+              name='sort'
+              id='sort'
+              className='border p-4 rounded-md'
+            >
+              <option value=''>Sort By Price</option>
               <option value='dsc'>Newest Product</option>
               <option value='asc'>Price Low to High</option>
               <option value='asc'>Price High to Low </option>
             </select>
           </div>
+
           <button onClick={handleReset} className='btn'>
             Reset
           </button>
         </div>
         <div className='grid justify-center grid-cols-1 gap-5 sm:grid-cols-3 lg:grid-cols-4 mt-8 xl:mt-8'>
-          {jobs.map(job => (
-            <ProductCard key={job._id} job={job} />
+          {products.map(product => (
+            <ProductCard key={product._id} product={product} />
           ))}
-           <ProductCard />
         </div>
       </div>
 
@@ -163,9 +235,8 @@ const Product = () => {
           <button
             onClick={() => handlePaginationButton(btnNum)}
             key={btnNum}
-            className={`hidden ${
-              currentPage === btnNum ? 'bg-[#51AA1B] text-white' : ''
-            } px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-[#51AA1B]  hover:text-white`}
+            className={`hidden ${currentPage === btnNum ? 'bg-[#51AA1B] text-white' : ''
+              } px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-[#51AA1B]  hover:text-white`}
           >
             {btnNum}
           </button>
