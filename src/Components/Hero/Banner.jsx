5cb204e21efaa395eb2273958dd659c87e62abb8
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-08-17 11:41:00 +0600

    added Hero section

--- src/Components/Hero/Banner.jsx
+++ src/Components/Hero/Banner.jsx
@@ -1,6 +1,6 @@
 import slider1 from '../../assets/images/slide.jpg'
-// import slider3 from '../../assets/images/slide.jpg'
-// import slider4 from '../../assets/images/slide.jpg'
+import slider2 from '../../assets/images/slide2.jpg'
+import slider3 from '../../assets/images/slide3.jpg'
 
 // Swiper
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -18,7 +18,7 @@ const Banner = () => {
             pagination={true}
             modules={[Navigation, Autoplay, EffectFade, Pagination]}
             autoplay={{
-                delay: 4000,
+                delay: 9000,
               }}
             loop={true}
         >
@@ -26,6 +26,39 @@ const Banner = () => {
 
             <SwiperSlide>
                 <section className="relative -z-50 bg-cover bg-fixed bg-center bg-no-repeat md:h-[620px] h-[500px]" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${slider1})` }}>
+
+                    <div
+                        className="absolute inset-0 sm:bg-transparent  sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
+                    >
+                    </div>
+
+                    <div
+                        className="relative lg:h-screen lg:items-center"
+                    >
+                        <div className='flex items-center justify-center md:h-[540px] h-[250px]'>
+                            <div className="mt-14 text-white mx-auto md:max-w-[820px] text-center ltr:sm:text-left rtl:sm:text-right p-10 rounded-lg">
+                                <i className="md:text-3xl text-sm mt-4max-w-2xl">
+                                    It has finally started
+                                </i>
+                                <h1 className="text-3xl my-5 font-bold md:text-7xl uppercase">
+                                    Huge Sale
+                                </h1>
+                                <p className="md:text-5xl text-sm mt-4 mb-10 max-w-2xl uppercase">
+                                    Up to 70% off
+                                </p>
+                                <div className='space-x-5'>
+                                    <button className='border border-white py-[9px] bg-transparent hover:bg-white hover:text-black px-6 font-semibold uppercase'> shop men </button>
+                                    <button className='border border-white py-[9px] bg-transparent hover:bg-white hover:text-black px-6 font-semibold uppercase'> shop women </button>
+                                    <button className='border border-white py-[9px] bg-transparent hover:bg-white hover:text-black px-6 font-semibold uppercase'> shop all </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </section>
+            </SwiperSlide>
+            <SwiperSlide>
+                <section className="relative bg-cover bg-fixed bg-center bg-no-repeat md:h-[620px] h-[500px]" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${slider2})` }}>
 
                     <div
                         className="absolute inset-0 sm:bg-transparent  sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
@@ -58,7 +91,7 @@ const Banner = () => {
                 </section>
             </SwiperSlide>
             <SwiperSlide>
-                <section className="relative bg-cover bg-fixed bg-center bg-no-repeat md:h-[620px] h-[500px]" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${slider1})` }}>
+                <section className="relative bg-cover bg-fixed bg-center bg-no-repeat md:h-[620px] h-[500px]" style={{ backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)), url(${slider3})` }}>
 
                     <div
                         className="absolute inset-0 sm:bg-transparent  sm:to-white/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
